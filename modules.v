8ad53ca3d642956ea8847967f71c1bed8d9b57f1
Samuele Capacci <96582680+VulcanRedEngineer1701@users.noreply.github.com>
2022-02-09 11:56:40 +0100

    update 2
    Non l'ho testato, non so neppure se "compili" (cioè se non dia errori di sorta per simulazione o simili), intanto invio il codice, aggiornerò successivamente
    Il testbench per questo non l'ho ancora pensato, lo invierò successivamente.

--- modules.v
+++ modules.v
@@ -152,3 +152,120 @@ module USBReader(input ck, reset, dataIn, clock, output word_ready, output[10:0]
     cnt34us dut3(ck,clr34us,hit34us);
 
 endmodule
+
+//---------------------------------------------------------------------------------------------------------
+//USB sender: (da debuggare)
+
+module writerSM(input ck, reset, send, inout data, clock, input hit10, hit01ms, input dataout, output reg en10, clr10, clr01ms, shift, busy);
+
+parameter[3:0] IDLE = 0, CLOCK0 = 1, CLOCK1 = 2, WAIT1 = 3, WAIT0 = 4, HIT10EN = 5, SHIFTDATA = 6, WAITFINAL1 = 7, WAITFINAL0 = 8;
+reg[3:0] currentState, nextState;
+
+reg reading_clock, reading_data;
+reg assignedClock;
+
+assign data = reading_data ? 1'bz : dataout;
+assign clock = reading_clock ? 1'bz : assignedClock;
+
+always @(posedge ck, posedge reset)
+if(reset) currentState <= IDLE;
+else currentState <= nextState;
+
+always @(*)
+case(currentState)
+IDLE:
+if(send) nextState = CLOCK0;
+else nextState = IDLE;
+CLOCK0:
+if(hit01ms) nextState = CLOCK1;
+else nextState = CLOCK0;
+CLOCK1:
+nextState = WAIT1;
+WAIT1:
+if(clock) nextState = WAIT1;
+else nextState = WAIT0;
+WAIT0:
+if(~clock) nextState = WAIT0;
+else nextState = HIT10EN;
+HIT10EN:
+nextState = SHIFTDATA;
+SHIFTDATA:
+if(~hit10) nextState = WAIT1;
+else nextState = WAITFINAL1;
+WAITFINAL1:
+if(clock) nextState = WAITFINAL1;
+else nextState = WAITFINAL0;
+WAITFINAL0:
+if(~clock) nextState = WAITFINAL0;
+else nextState = IDLE;
+default:nextState=IDLE;
+endcase
+
+always @(*)
+case(currentState)
+IDLE: {reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b11x0110;
+CLOCK0: {reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b0000100;
+CLOCK1:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b0010110;
+WAIT1:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b10x0010;
+WAIT0:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b10x0010;
+HIT10EN:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b10x1010;
+SHIFTDATA: {reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b10x0011;
+WAITFINAL1:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b11x0110;
+WAITFINAL0: {reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b11x0110;
+default:{reading_clock, reading_data, assignedClock, en10, clr10, clr01ms, shift} = 7'b11x0110;
+endcase
+
+//questo l'ho aggiunto dopo, funziona uguale
+always @(*)
+if(currentState == IDLE) busy = 0;
+else busy = 1;
+
+endmodule
+
+module counter10(input ck, reset, en, output reg hit);
+reg[3:0] count, countNext;
+
+always @(posedge ck, posedge reset)
+if(reset) count = 0;
+else count = countNext;
+
+always @(*)
+if(en && count < 10) countNext = count + 1;
+else countNext = count;
+
+always @(*)
+if(count == 10) hit = 1;
+else hit = 0;
+
+endmodule
+
+module counter01ms(input ck, reset, output reg hit);
+reg[15:0]count;
+
+always @(posedge ck, posedge reset)
+if(reset) count <= 0;
+else if(count < 10000) count <= count + 1;
+
+endmodule
+
+module serializer(input ck, shift, save, input[9:0] dataToSend, output dataout);
+reg[9:0] savedData;
+
+assign dataout = savedData[9];
+
+always @(posedge ck)
+if(save) savedData = dataToSend;
+else if(shift) savedData = {savedData[8:0],1'bx};
+
+endmodule
+
+
+module USBsender(input ck, reset, send, input[9:0] dataToSend, inout data, clock, output busy);
+wire hit10, hit01ms, en10, shift, clr10, clr01ms, currentdata;
+
+writerSM x0(ck, reset, data, clock, hit10, hit01ms, currentdata, en10, clr10, clr01ms, shift, busy);
+counter10 x1(ck, reset || clr10, en10, hit10);
+counter01ms x2(ck, reset || clr01ms, hit01ms);
+serializer x3(ck, shift, send && ~busy, dataToSend, currentdata); //salva quando siamo in idle e viene inviato un nuovo segnale di send
+
+endmodule
